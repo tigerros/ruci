ce507e39980a5eb4648dc395e0f13d1c0dba92b9
tigerros <tigerros.gh@gmail.com>
2024-09-22 20:33:08 +0200

    init

--- src/uci_connection.rs
+++ src/uci_connection.rs
@@ -319,3 +319,25 @@ fn update_id(old_id: &mut Option<IdMessageKind>, new_id: IdMessageKind) {
         }
     });
 }
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[tokio::test]
+    async fn skip_lines() {
+        println!("{:?}", std::env::current_dir().unwrap());
+        let mut engine_conn = EngineConnection::from_path("/resources/stockfish.exe").unwrap();
+
+        engine_conn.send_message(&GuiMessage::UseUci).await.unwrap();
+
+        engine_conn.skip_lines(4).await.unwrap();
+
+        let mut line = String::new();
+        engine_conn.stdout.read_line(&mut line).await.unwrap();
+
+        assert_eq!(line, "option name Debug Log File type string default\n");
+    }
+}
