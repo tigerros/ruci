7e4ce083b72bdd91ce9d978c765e8de88bcac0d3
tigerros <aurel.leonard.danel@gmail.com>
2025-03-18 14:18:25 +0100

    fmt

--- examples/no_std.rs
+++ examples/no_std.rs
@@ -7,8 +7,11 @@ use core::str::FromStr;
 use ruci::gui::Go;
 
 fn main() {
-    assert_eq!(Go::from_str("go depth 17"), Ok(Go {
-        depth: Some(17),
-        ..Default::default()
-    }));
+    assert_eq!(
+        Go::from_str("go depth 17"),
+        Ok(Go {
+            depth: Some(17),
+            ..Default::default()
+        })
+    );
 }
